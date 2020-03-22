c32035ed3e7ef257b4e9ffe4cf2f52aef7eb0108
Alexander Hagbarth <ahagbarth@gmail.com>
2020-03-22 16:19:47 +0100

    added elements to navbar

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,16 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          <b-nav-item active href="/">Anime</b-nav-item>
+          <b-nav-item href="/about">Manga</b-nav-item>
         </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" variant="outline-light" type="submit">Search</b-button>
+        </b-nav-form>
+      </b-navbar-nav>
       </b-collapse>
     </b-navbar>
     <router-view />
