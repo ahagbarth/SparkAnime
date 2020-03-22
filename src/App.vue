9f8f6cffce7706c275c27bc18e5d3bac427e8a17
Alexander Hagbarth <ahagbarth@gmail.com>
2020-03-22 05:42:22 +0100

    added unfinished navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    </div> -->
+    <b-navbar toggleable="lg" type="dark">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <router-view />
   </div>
 </template>
 
@@ -15,8 +26,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: #121212;
- 
+  background-color: #212121;
 }
 
 #nav {
@@ -30,5 +40,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+.navbar{
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+  z-index: 6;
 }
 </style>
