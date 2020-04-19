2172da9d3c9eb9f2f93110541afc8da9f0cb5b24
Alexander Hagbarth <ahagbarth@gmail.com>
2020-04-19 18:43:06 +0200

    Modified navbar to set active tab

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,39 @@
       <router-link to="/about">About</router-link>
     </div> -->
     <b-navbar toggleable="lg" sticky="true" type="dark">
-       <router-link to="/"><b-navbar-brand href="">SparkAnime</b-navbar-brand></router-link>
+      <router-link to="/"
+        ><b-navbar-brand href="">SparkAnime</b-navbar-brand></router-link
+      >
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item active href="/">Anime</b-nav-item>
-          <b-nav-item href="/about">Manga</b-nav-item>
+          <b-nav-item to="/" exact exact-active-class="active"
+            >Anime</b-nav-item
+          >
+          <b-nav-item to="/manga" exact exact-active-class="active"
+            >Manga</b-nav-item
+          >
         </b-navbar-nav>
 
-      <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2 inputSearch " placeholder="Search" trim></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0 searchComponent" style="background-color:#FF5116; border-color:#FF5116" variant="outline-light" type="submit">Search</b-button>
-        </b-nav-form>
-      </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2 inputSearch "
+              placeholder="Search"
+              trim
+            ></b-form-input>
+            <b-button
+              size="sm"
+              class="my-2 my-sm-0 searchComponent"
+              style="background-color:#FF5116; border-color:#FF5116"
+              variant="outline-light"
+              type="submit"
+              >Search</b-button
+            >
+          </b-nav-form>
+        </b-navbar-nav>
       </b-collapse>
     </b-navbar>
     <router-view />
@@ -35,7 +53,6 @@
   color: #2c3e50;
   background-color: #181818;
   font-family: MonuMonumentExtended-regular;
-
 }
 
 #nav {
@@ -50,20 +67,17 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
-.navbar{
+.navbar {
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
   z-index: 6;
 }
 
 @font-face {
   font-family: MonumentExtended-bold;
-  src:  url(/assets/fonts/MonumentExtended-Ultrabold.otf) format('otf');
-} 
+  src: url(/assets/fonts/MonumentExtended-Ultrabold.otf) format("otf");
+}
 @font-face {
   font-family: MonumentExtended-regular;
-  src: url(/assets/fonts/MonumentExtended-regular.otf) format('otf')
-
+  src: url(/assets/fonts/MonumentExtended-regular.otf) format("otf");
 }
-
-
 </style>
